aa425a548a5de7e497f48f3fdd73ab3984a20b13
Lexi Blanchet <lexi.blanchet@outlook.com>
2025-07-28 16:48:44 -0600

    Create a more graceful shutdown

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -22,20 +22,8 @@ if [ -e "/config/SandboxSettings.ini" ]; then
     fi
 fi
 
-# Function to handle shutdown
-shutdown() {
-    echo "Caught SIGTERM, shutting down application..."
-    pkill --signal TERM --parent $APPLICATION_PID
-    wait "$APPLICATION_PID"
-    echo "Application gracefully stopped."
-    exit 0
-}
-
-# Trap the SIGTERM signal and call the shutdown function
-trap 'shutdown' TERM
-
 # The order is VERY particular and setup is sourced from https://github.com/DFJacob/AbioticFactorDedicatedServer/issues/3#issuecomment-2094369127
-xvfb-run wine /server/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread \
+xvfb-run -a wine /server/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shipping.exe -log -newconsole -useperfthreads -NoAsyncLoadingThread \
     -MaxServerPlayers=${MAXPLAYERS} \
     -PORT=${PORT} -QUERYPORT=${QUERYPORT} \
     -SteamServerName=${SERVERNAME} \
@@ -43,5 +31,17 @@ xvfb-run wine /server/AbioticFactor/Binaries/Win64/AbioticFactorServer-Win64-Shi
     -ServerPassword=$ServerPassword &
 APPLICATION_PID=$!
 
+# Function to handle shutdown
+shutdown() {
+    echo "Caught SIGTERM, shutting down application..."
+    kill -"$1" $(pgrep -P $APPLICATION_PID)
+    wait $APPLICATION_PID
+    echo "Application gracefully stopped."
+    exit 0
+}
+
+# Trap the SIGTERM signal and call the shutdown function
+trap 'shutdown SIGTERM' SIGTERM
+
 # Wait for the application to finish (or for the signal to be caught)
-wait "$APPLICATION_PID"
+wait $APPLICATION_PID
